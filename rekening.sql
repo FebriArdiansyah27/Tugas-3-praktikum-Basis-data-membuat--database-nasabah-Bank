845a8ddd066d22a8b8672565929c1042ac77989e
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 00:48:27 +0700

    Update rekening.sql

--- rekening.sql
+++ rekening.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE Rekening (
+    Nomor_rekening INT(18) PRIMARY KEY,
+    Saldo INT(18),
+    Jenis_rekening ENUM('Giro', 'Deposito', 'Tabungan'),
+    Tanggal_dibuka DATE,
+    Jumlah_transaksi INT(18)
+);
