645795302ad8dacf3fc65cca186932e0662a9769
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 00:46:48 +0700

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,1 +1,10 @@
 
+CREATE TABLE Bank (
+    Id_bank INT PRIMARY KEY,
+    Nama_bank VARCHAR(20),
+    Nomor_rekening INT,
+    Alamat_kantor_pusat VARCHAR(30),
+    Nomor_direktur INT(30),
+    No_telepon_kantor_pusat INT(30),
+    FOREIGN KEY (Nomor_rekening) REFERENCES NRekenin(Nomor_Rekening)
+);
