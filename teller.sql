e8e671685533698f46be7eb0c95fcb7198e3d68f
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 00:50:27 +0700

    Update teller.sql

--- teller.sql
+++ teller.sql
@@ -1,1 +1,6 @@
-t
+CREATE TABLE Teller (
+    Id_teller INT(14) PRIMARY KEY,
+    Id_direktur INT(14),
+    Nama_teller VARCHAR(30),
+    FOREIGN KEY (Id_direktur) REFERENCES Direktur(Id_direktur)
+);
