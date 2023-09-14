2f2ce4fee26ad6d91476aaf23b8b65421db10e8f
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 00:52:36 +0700

    Update transaksi.sql

--- transaksi.sql
+++ transaksi.sql
@@ -1,1 +1,11 @@
+CREATE TABLE Transaksi (
+    Nomor_transaksi INT(20) PRIMARY KEY,
+    Nomor_rekening INT(18),
+    Keterangan_transaksi VARCHAR(20),
+    Jenis_transaksi VARCHAR(40),
+    Tanggal_transaksi DATE,
+    Jumlah_transaksi INT(125),
+    Saldo INT(125),
+    FOREIGN KEY (Nomor_rekening) REFERENCES Rekening(Nomor_rekening)
+);
 
