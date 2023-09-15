ea43e48bb95c77ba4f53f4c39123fcb1a1e7a8af
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 14:19:00 +0700

    Update direktur.sql

--- direktur.sql
+++ direktur.sql
@@ -1,7 +1,7 @@
 
-CREATE TABLE Direktur (
-    Id_direktur INT(14) PRIMARY KEY,
-    Nama_direktur VARCHAR(40),
-    Jenis_kelamin ENUM('Laki-Laki', 'Perempuan'),
-    Nomor_telepon INT(14)
+CREATE TABLE Direktur (      --- membuat table baru bernama Direktur
+    Id_direktur INT(14) PRIMARY KEY, --  menambahkan kolom id_direktur sebagai primary key
+    Nama_direktur VARCHAR(40),            -- menambahkan kolom nama direkur
+    Jenis_kelamin ENUM('Laki-Laki', 'Perempuan'),  -- menambahkan kolom jenis kelamin dnegan memnggunakan fungsi enum untuk membatasi value yang bisa diinputkan
+    Nomor_telepon INT(14)               -- menambahkan kolom nomor telepon
 );
