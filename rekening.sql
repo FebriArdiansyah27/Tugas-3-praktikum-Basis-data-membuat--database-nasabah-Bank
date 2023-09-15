0db32141efe1b447958b9cd0da5e106ad89abcc6
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 14:12:59 +0700

    Update rekening.sql

--- rekening.sql
+++ rekening.sql
@@ -1,7 +1,9 @@
-CREATE TABLE Rekening (
-    Nomor_rekening INT(18) PRIMARY KEY,
-    Saldo INT(18),
-    Jenis_rekening ENUM('Giro', 'Deposito', 'Tabungan'),
-    Tanggal_dibuka DATE,
-    Jumlah_transaksi INT(18)
+CREATE TABLE Rekening (                 -- menambahkan sebuah tabel baru bernama Rekening
+    Nomor_rekening INT(18) PRIMARY KEY, --menambahkan  kolom Nomor_rekening sebagai primay key
+    Saldo INT(18),                      --menambahkan  kolom saldo 
+    Jenis_rekening ENUM('Giro', 'Deposito', 'Tabungan'), --menambahkan  kolom Jenis rekening, disini kita 
+                                                          --menggunakan fungsi Enum yang artinya kita harus memilih jenis rekening antara tabungan,
+                                                          --giro dan deposito 
+    Tanggal_dibuka DATE,           -- menambahkan kolom tanggal dibukanya rekening
+    Jumlah_transaksi INT(18)        -- menambahkan kolom jumlah transaksi
 );
