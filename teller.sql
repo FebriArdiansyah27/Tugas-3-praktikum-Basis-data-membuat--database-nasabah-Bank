8e5bfc2cb32a44333ffc70cd0f35ba212e7e7267
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 14:07:31 +0700

    Update teller.sql

--- teller.sql
+++ teller.sql
@@ -1,6 +1,5 @@
-CREATE TABLE Teller (
-    Id_teller INT(14) PRIMARY KEY,
-    Id_direktur INT(14),
-    Nama_teller VARCHAR(30),
-    FOREIGN KEY (Id_direktur) REFERENCES Direktur(Id_direktur)
+CREATE TABLE Teller (                 --menambahkan  tabel baru bernama teller
+    Id_teller INT(14) PRIMARY KEY,    --menambahkan  kolom id_teller yang berperan sebagai primary key
+    Nama_teller VARCHAR(30),          --menambahkan  kolom Nama teller
+    FOREIGN KEY (Id_direktur) REFERENCES Direktur(Id_direktur)     --menambahkan  kolom  id direktur sebagai Foreign key
 );
