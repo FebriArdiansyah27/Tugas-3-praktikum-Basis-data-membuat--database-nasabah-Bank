3381904d3f9a085b83e777f9bda5cb456ee3b4b4
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 14:22:02 +0700

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -1,10 +1,9 @@
 
-CREATE TABLE Bank (
-    Id_bank INT PRIMARY KEY,
-    Nama_bank VARCHAR(20),
-    Nomor_rekening INT,
-    Alamat_kantor_pusat VARCHAR(30),
-    Nomor_direktur INT(30),
-    No_telepon_kantor_pusat INT(30),
-    FOREIGN KEY (Nomor_rekening) REFERENCES NRekenin(Nomor_rekening)
+CREATE TABLE Bank (        --membuat tabel baru bernama Bank
+    Id_bank INT PRIMARY KEY,    -- menambahkan id bank sebagai primary key 
+    Nama_bank VARCHAR(20),       -- menambahkan kolom nama bank
+    Alamat_kantor_pusat VARCHAR(30),    -- menambahkan kolom alamat kantor pusat
+    Nomor_direktur INT(30),                -- menambahkan kolom nomor direktur
+    No_telepon_kantor_pusat INT(30),         -- menambahkan kolom  nomor telepon kantor pusat
+    FOREIGN KEY (Nomor_rekening) REFERENCES NRekenin(Nomor_rekening)   -- menambahkan kolom nomor rekening sebagai foreign key
 );
