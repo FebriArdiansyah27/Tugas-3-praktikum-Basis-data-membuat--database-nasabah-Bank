a000aa0eb055045733bac6ad4f1d19c502f2bb38
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 14:24:52 +0700

    Update transaksi.sql

--- transaksi.sql
+++ transaksi.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Transaksi (                  --membuat tabel baru bernama Transaksi
     Nomor_transaksi INT(20) PRIMARY KEY,  --menambahkan  kolom  Nomor_transaksi sebagai primary key
     Keterangan_transaksi VARCHAR(20),     -- menambahkan kolong keterangan transaksi 
-    Jenis_transaksi VARCHAR(40),           --menambahkan  kolom jenis transaksi
+    Jenis_transaksi ENUM('tarik tunai','setor tunai', 'tranfer tunai'),           --menambahkan  kolom jenis transaksi
     Tanggal_transaksi DATE,                --menambahkan  kolom tanggal transaksi
     Jumlah_transaksi INT(125),            --menambahkan  kolom menmabahkan jumlah transaksi
     Saldo INT(125),                       --menambahkan  kolom saldo
