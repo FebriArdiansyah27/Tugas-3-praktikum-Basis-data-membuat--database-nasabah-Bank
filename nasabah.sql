95b5487f3c0af2deaff1506b0843b74c4a728822
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-15 14:16:50 +0700

    Update nasabah.sql

--- nasabah.sql
+++ nasabah.sql
@@ -1,14 +1,12 @@
-CREATE TABLE nasabah (
-    Id_nasabah INT(20) PRIMARY KEY,
-    Id_teller INT(20),
-    Nomor_rekening INT(20),
-    Nama VARCHAR(35),
-    Tanggal_lahir DATE,
-    Email VARCHAR(35),
-    Nomor_telepon INT(14),
-    Alamat VARCHAR(40),
-    Jenis_kelamin ENUM('Laki-Laki', 'Perempuan'),
-    FOREIGN KEY (Id_teller) REFERENCES Teller(Id_teller),
-    FOREIGN KEY (Nomor_rekening) REFERENCES Rekening(Nomor_Rekening)
+CREATE TABLE nasabah (    -- membuat tabel baru bernama nasabah
+    Id_nasabah INT(20) PRIMARY KEY,  -- menambahkan kolom id_nasabah sebgai Primay key
+    Nama VARCHAR(35),                 -- menambahkan kolom nama
+    Tanggal_lahir DATE,                -- menambahkan kolom tanggal lahir
+    Email VARCHAR(35),                  -- menambahkan kolom email
+    Nomor_telepon INT(14),               -- menambahkan kolom nomor telepon
+    Alamat VARCHAR(40),                   -- menambahkan kolom Alamat
+    Jenis_kelamin ENUM('Laki-Laki', 'Perempuan'),  -- menambahkan kolom Jenis kelamin, dengan menggunakan fungsi enum supaya bisa langsung memilih value
+    FOREIGN KEY (Id_teller) REFERENCES Teller(Id_teller),  -- menambahkan kolom id_teller sebagai foreign key
+    FOREIGN KEY (Nomor_rekening) REFERENCES Rekening(Nomor_Rekening)    -- menambahkan kolom nomor_rekening sebagai foreign key
 );
 
