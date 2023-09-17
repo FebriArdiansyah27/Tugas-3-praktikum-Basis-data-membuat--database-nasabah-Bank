11d87d39f0f5f473e378d296b1190af50d3f6051
FebriArdiansyah27 <131425009+FebriArdiansyah27@users.noreply.github.com>
2023-09-17 16:57:37 +0700

    Update teller.sql

--- teller.sql
+++ teller.sql
@@ -2,4 +2,9 @@ CREATE TABLE Teller (                 --menambahkan  tabel baru bernama teller
     Id_teller INT(14) PRIMARY KEY,    --menambahkan  kolom id_teller yang berperan sebagai primary key
     Nama_teller VARCHAR(30),          --menambahkan  kolom Nama teller
     FOREIGN KEY (Id_direktur) REFERENCES Direktur(Id_direktur)     --menambahkan  kolom  id direktur sebagai Foreign key
+    Jenis_kelamin_teller ENUM('Laki-laki', 'Perempuan'),
+    Nomor_telepon INT(14),
+    Tanggal_lahir DATE,
+    Alamat VARCHAR(40),
+    Tanggal_bergabung DATE
 );
